9bcce913a113ed0821d33537e26d8060b4b297ce
Zigreal <grigoriy.nov1998@gmail.com>
2022-01-02 17:28:31 +0300

    A prototype of the dice game has been created. To win, you need to score 10 or more points by throwing 3 dice. Implemented a reward system: for matching two values or a complete match of values, you get extra points.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Random dice = new Random();
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+int total = roll1 + roll2 + roll3;
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("Congratulations! All the dropped values are the same, 6 points are added to you!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("Congratulations! Two identical values have fallen out, 2 points have been added to you!");
+        total += 2;
+    }
+}
+
+Console.WriteLine($"The result is {total}");
+
+if (total > 9)
+{
+    Console.WriteLine("You're win!");
+}
+else
+{
+    Console.WriteLine("You're lose!");
+}
