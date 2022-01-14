e93626cb2bebb9765f3a4559dcbc21a36de92b8a
Zigreal <grigoriy.nov1998@gmail.com>
2022-01-15 02:41:50 +0300

    puts duplicate code in a method PrintNumber

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
 ﻿using System.Threading;
 
+static void PrintNumber() {
+    for(int i = 3; i > 0; i--) {
+        Console.WriteLine($"{i}");
+        Thread.Sleep(1000);
+    }
+}
+
 Random dice = new Random();
 int roll1 = dice.Next(1,7);
 int roll2 = dice.Next(1,7);
 int roll3 = dice.Next(1,7);
 int total = roll1 + roll2 + roll3;
 
-
-Console.WriteLine("3");
-Thread.Sleep(1000);
-Console.WriteLine("2");
-Thread.Sleep(1000);
-Console.WriteLine("1");
-Thread.Sleep(1000);
-
+PrintNumber();
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
